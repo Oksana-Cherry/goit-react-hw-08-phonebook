3eba03fa23fd4224568ba5b7ac466a88d2cef477
Oksana Cherepanova <oksa-lera@ukr.net>
2021-04-23 19:06:11 +0300

    Updates

--- static/js/5.04bf9fbb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react"]=this["webpackJsonpgoit-react"]||[]).push([[5],{135:function(t,e,n){"use strict";n.r(e);n(0);var i=n(2),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};e.default=function(){return Object(i.jsx)("div",{style:c.container,children:Object(i.jsx)("h1",{style:c.title,children:"Phonebook "})})}}}]);
-//# sourceMappingURL=5.04bf9fbb.chunk.js.map
